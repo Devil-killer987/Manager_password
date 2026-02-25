783d372074717dcbf94491fc5488b096b213260d
Zhdanov Danil <a.luppov@mail.ru>
2026-02-25 13:10:31 +0300

    Добавьте файлы проекта.

---
+++ AppDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager_password
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=mydatabase.db");
+        }
+
+        public void EnsureDatabaseCreated()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
